16792b6f6dac6e8df8d68d05e3de9338987a6d47
Antonio <antonio.jpsalves@gmail.com>
2022-07-11 19:19:39 -0300

    iniciando com a rota de post

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import express from 'express';
 // criando uma instância de express
 const app = express();
 
+// recurso que interpreta o que chega em forma de json no request
+app.use(express.json());
+
 const livros = [
     {
         id: 1,
@@ -14,13 +17,23 @@ const livros = [
     }
 ];
 
+// criando uma rota para request tipo get '/' 
 app.get('/', (req, res) => {
     res.status(200).send('Curso de node js!');
 });
 
+// criando uma rota para request tipo get '/livros' 
 app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
 
+// criando uma rota para requst do tipo post
+app.post('/livros', (req, res) => {
+    // incluindo no array o que vier no corpo da requst
+    livros.push(req.body);
+
+    //devolvendo uma respota
+    res.status(201).send('Livro cadastrado com sucesso');
+});
 
 export default app;
